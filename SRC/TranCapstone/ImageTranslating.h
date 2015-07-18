584310b0fcd7ad1e589a3a5f5ee63a85ba26cc92
nicknguyen071093 <nicknguyen071093@gmail.com>
2015-07-18 22:29:38 +0700

    Long - fix githup

--- SRC/TranCapstone/ImageTranslating.h
+++
@@ -1,9 +0,0 @@
-#ifndef IMAGETRANSLATING_H
-#define IMAGETRANSLATING_H
-
-#include <QImage>
-#include <opencv2/core/core.hpp>
-
-QImage  cvMatToQImage( const cv::Mat &inMat );
-
-#endif // IMAGETRANSLATING_H
